9668ca07d3fc2fca93246395e472bde90f5f4c86
Clay McKee <cmckee120993@gmail.com>
2022-09-13 19:38:36 +0200

    used .join to bring the objects in the array together; now supplies readable html to fs; cleaned up unnecessary code.

--- createSite.js
+++ createSite.js
@@ -1,10 +1,27 @@
-const Intern = require('./lib/Intern');
-const Manager = require ('./lib/Manager');
-const Engineer = require('./lib/Engineer');
+const Intern = require("./lib/Intern");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
 
 function createSite(data) {
-const generateManager = manager => {
-        return `<article class="card col-md-4 mb-3">
+  const generateManager = (manager) => {
+    return `<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+        <link rel="stylesheet" href="./style.css">
+        <title>My Team</title>
+    </head>
+    <body>
+        <header class="container-fluid text-center p-5">
+            <h1>My Team</h1>
+        </header>
+
+        <main class="container text-center p-3">
+        <section class="row">
+        
+    <article class="card col-md-4 mb-3">
                     <div class="card" style="width: 18rem;">
                         <div class="card-header">
                             <h2 class="card-title">${manager.getName()}</h2>
@@ -16,11 +33,11 @@ const generateManager = manager => {
                             <li class="list-group-item">Office: ${manager.getOfficeNumber()}</li>
                         </ul>
                     </div> 
-                </article>`
-    };
+                </article>`;
+  };
 
-    const generateEngineer = engineer => {
-      return `<article class="card col-md-4 mb-3">
+  const generateEngineer = (engineer) => {
+    return `<article class="card col-md-4 mb-3">
                     <div class="card" style="width: 18rem;">
                          <div class="card-header">
                             <h2 class="card-title">${engineer.getName()}</h2>
@@ -33,10 +50,10 @@ const generateManager = manager => {
                         </ul>
                     </div>
                 </article>`;
- };
+  };
 
-const generateIntern = intern => {
-        return `<article class="card col-md-4 mb-3">
+  const generateIntern = (intern) => {
+    return `<article class="card col-md-4 mb-3">
                      <div class="card" style="width: 18rem;">
                         <div class="card-header">
                             <h2 class="card-title">${intern.getName()}</h2>
@@ -49,147 +66,39 @@ const generateIntern = intern => {
                         </ul>
                     </div>
                 </article>`;
-};
+  };
 
+  const endHTML = `   </section>
 
-const html = []
+  </main>
 
-html += (data.filter(employee => employee.getRole() === 'Manager').map(manager => generateManager(manager)));
-html += (data.filter(employee => employee.getRole() === 'Engineer').map(engineer => generateEngineer(engineer)));
-html += (data.filter(employee => employee.getRole() === 'Intern').map(intern => generateIntern(intern)));
+</body>
+</html>`
 
-console.log(typeof html);
-console.log (JSON.stringify(html));
-};
-// const generateCards = teamArray => {
-//     let cardsArray = [];
-//     const managerTitle = teamArray.filter(team => {
-//         return team.getRole() === 'Manager';
-//     });
-//     const engineerTitle = teamArray.filter(team => {
-//         return team.getRole() === 'Engineer';
-//     });
-//     const internTitle = teamArray.filter(team => {
-//         return team.getRole() === 'Intern';
-//     });
+  const html = [];
 
-//     if (managerTitle) {
-//         cardsArray.push(generateManager(managerTitle));
-//     };
-//     if (engineerTitle) {
-//         cardsArray.push(generateEngineer(engineerTitle));
-//     };
-//     if (internTitle) {
-//         cardsArray.push(generateIntern(internTitle));
-//     };
-//     return cardsArray.join('');
-// }
+  html.push(
+    data
+      .filter((employee) => employee.getRole() === "Manager")
+      .map((manager) => generateManager(manager))
+      .join("")
+  );
+  html.push(
+    data
+      .filter((employee) => employee.getRole() === "Engineer")
+      .map((engineer) => generateEngineer(engineer))
+      .join("")
+  );
+  html.push(
+    data
+      .filter((employee) => employee.getRole() === "Intern")
+      .map((intern) => generateIntern(intern))
+      .join("")
+  );
+  html.push(endHTML);
 
-// function createSite(data) {
-//     console.log(data);
-//      let userSite =  
-//         `<html lang="en">
-//         <head>
-//             <meta charset="utf-8">
-//             <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-        
-//             <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-//             <link rel="stylesheet" href="./assets/style.css">
-//             <title>My Team</title>
-//         </head>
-//         <body>
-//             <header class="container-fluid text-center p-5">
-//                 <h1>My Team</h1>
-//             </header>
-        
-//             <main class="container text-center p-3">
-//             <section class="row">
-        
-//                 <article class="card col-md-4 mb-3">
-//                     <div class="card" style="width: 18rem;">
-//                         <div class="card-header">
-//                         <h2 class="card-title">${data[0].name}</h2>
-//                         <h3 class="card-title">Manager</h3>
-//                         </div>
-//                         <ul class="list-group list-group-flush">
-//                         <li class="list-group-item">ID: ${data[0].id}</li>
-//                         <li class="list-group-item">Email: ${data[0].email}</li>
-//                         <li class="list-group-item">Office: ${data[0].officeNumber}</li>
-//                         </ul>
-//                     </div>
-//                 </article>`
-
-//             if (data[1] === Engineer) {
-//                 for (i=0; i<data[1].length; i++) {
-//                   userSite +=  `<article class="card col-md-4 mb-3">
-//                     <div class="card" style="width: 18rem;">
-//                         <div class="card-header">
-//                         <h2 class="card-title">${data[1].getName()}</h2>
-//                         <h3 class="card-title">Engineer</h3>
-//                         </div>
-//                         <ul class="list-group list-group-flush">
-//                         <li class="list-group-item">ID: ${data[1].getID()}</li>
-//                         <li class="list-group-item">Email: ${data[i].getEmail()}</li>
-//                         <li class="list-group-item">GitHub: ${data[1].getGitHub()}</li>
-//                         </ul>
-//                     </div>
-//                 </article>`
-//                 };
-//             };
-
-//             if (data[2] === Intern) {
-//                 for (i=0; i<data[2].length; i++) {
-//                   userSite +=  `<article class="card col-md-4 mb-3">
-//                     <div class="card" style="width: 18rem;">
-//                         <div class="card-header">
-//                         <h2 class="card-title">${data[2].getName()}</h2>
-//                         <h3 class="card-title">Intern</h3>
-//                         </div>
-//                         <ul class="list-group list-group-flush">
-//                         <li class="list-group-item">ID: ${data[2].getID()}</li>
-//                         <li class="list-group-item">Email: ${data[2].getEmail()}</li>
-//                         <li class="list-group-item">GitHub: ${data[2].getSchool()}</li>
-//                         </ul>
-//                     </div>
-//                 </article>`
-//                 };
-//             };
-                
-//         userSite +=
-//             `</section>    
-            
-//             </main>
-        
-//         </body>
-//         </html>
-//         `
-//     };
+  return html.join('');
+}
 
 module.exports = createSite;
 
-// cardsArray => {
-//     `<html lang="en">
-//         <head>
-//         <meta charset="utf-8">
-//         <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-            
-//         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
-//         <link rel="stylesheet" href="./assets/style.css">
-//         <title>My Team</title>
-//         </head>
-            
-//         <body>
-//             <header class="container-fluid text-center p-5">
-//                 <h1>My Team</h1>
-//             </header>
-            
-//             <main class="container text-center p-3">
-//                 <section class="row">
-//                     ${generateCards(cardsArray)}
-//                 </section>    
-            
-//             </main>
-                        
-//         </body>
-//     </html>`
-// };
--- index.js
+++ index.js
@@ -1,159 +1,149 @@
-const inquirer = require('inquirer');
-const Manager = require('./lib/Manager');
-const Intern =require('./lib/Intern');
-const Engineer = require('./lib/Engineer');
-const Employee = require('./lib/Employee');
-const fs = require('fs');
-const createSite = require('./createSite');
-const path = require('path');
+const inquirer = require("inquirer");
+const Manager = require("./lib/Manager");
+const Intern = require("./lib/Intern");
+const Engineer = require("./lib/Engineer");
+const Employee = require("./lib/Employee");
+const fs = require("fs");
+const createSite = require("./createSite");
+const path = require("path");
 
-const teamArray =[];
-// const engineers =[];
-// const interns =[];
+const teamArray = [];
 
 class Prompt {
-    constructor() {
-        this.teamArray = [];
-    };
-    getTeamArray() {
-        return this.teamArray;
-    };
-    createManager() {
-        inquirer
-            .prompt([
-                {type: 'input',
-                name: 'name',
-                message: 'What is the manager\'s name?'
-                },
+  constructor() {
+    this.teamArray = [];
+  }
+  getTeamArray() {
+    return this.teamArray;
+  }
+  createManager() {
+    inquirer
+      .prompt([
+        { type: "input", name: "name", message: "What is the manager's name?" },
 
-                {type: 'input',
-                name: 'id',
-                message: 'What is the manager\'s id?'
-                },
+        { type: "input", name: "id", message: "What is the manager's id?" },
 
-                {type: 'input',
-                name: 'email',
-                message: 'What is the manager\'s email?'
-                },
+        {
+          type: "input",
+          name: "email",
+          message: "What is the manager's email?",
+        },
 
-                {type: 'input',
-                name: 'officeNumber',
-                message: 'What is the manager\'s office number?'
-                },
-            ]).then(function(answers) {
-                console.log(answers);
-                const newManager = new Manager(answers.id, answers.email, answers.name, answers.officeNumber);
-                teamArray.push(newManager);
-                createTeam();
-            });
-            
-        };
-};
+        {
+          type: "input",
+          name: "officeNumber",
+          message: "What is the manager's office number?",
+        },
+      ])
+      .then(function (answers) {
+        const newManager = new Manager(
+          answers.id,
+          answers.email,
+          answers.name,
+          answers.officeNumber
+        );
+        teamArray.push(newManager);
+        createTeam();
+      });
+  }
+}
 
 function createIntern() {
-    inquirer
-         .prompt([
-            {type: 'input',
-            name: 'name',
-            message: 'What is the intern\'s name?'
-            },
+  inquirer
+    .prompt([
+      { type: "input", name: "name", message: "What is the intern's name?" },
 
-            {type: 'input',
-            name: 'id',
-            message: 'What is the intern\'s id?'
-            },
+      { type: "input", name: "id", message: "What is the intern's id?" },
 
-            {type: 'input',
-            name: 'email',
-            message: 'What is the interns\'s email?'
-            },
+      { type: "input", name: "email", message: "What is the interns's email?" },
 
-            {type: 'input',
-            name: 'school',
-            message: 'What is the intern\'s school?'
-            },
-            ]).then(function(answers) {
-            console.log(answers);
-            const {id, email, name, school} = answers;
-            const newIntern = new Intern(answers.id, answers.email, answers.name, answers.school);
-            teamArray.push(newIntern);
-            
+      {
+        type: "input",
+        name: "school",
+        message: "What is the intern's school?",
+      },
+    ])
+    .then(function (answers) {
+      const { id, email, name, school } = answers;
+      const newIntern = new Intern(
+        answers.id,
+        answers.email,
+        answers.name,
+        answers.school
+      );
+      teamArray.push(newIntern);
 
-            createTeam();
-            })
-    };
+      createTeam();
+    });
+}
 
 function createEngineer() {
-    inquirer
-         .prompt([
-            {type: 'input',
-            name: 'name',
-            message: 'What is the engineer\'s name?'
-            },
+  inquirer
+    .prompt([
+      { type: "input", name: "name", message: "What is the engineer's name?" },
 
-            {type: 'input',
-            name: 'id',
-            message: 'What is the engineer\'s id?'
-            },
+      { type: "input", name: "id", message: "What is the engineer's id?" },
 
-            {type: 'input',
-            name: 'email',
-            message: 'What is the engineer\'s email?'
-            },
+      {
+        type: "input",
+        name: "email",
+        message: "What is the engineer's email?",
+      },
 
-            {type: 'input',
-            name: 'gitHub',
-            message: 'What is the engineer\'s GitHub?'
-            },
-        ]).then(function(answers) {
-            console.log(answers);
-            const {id, email, name, gitHub} = answers;
-            const newEngineer = new Engineer(answers.id, answers.email, answers.name, answers.gitHub);
-            teamArray.push(newEngineer);
-            createTeam();
-        })
-    };
+      {
+        type: "input",
+        name: "gitHub",
+        message: "What is the engineer's GitHub?",
+      },
+    ])
+    .then(function (answers) {
+      const { id, email, name, gitHub } = answers;
+      const newEngineer = new Engineer(
+        answers.id,
+        answers.email,
+        answers.name,
+        answers.gitHub
+      );
+      teamArray.push(newEngineer);
+      createTeam();
+    });
+}
 
 function createTeam() {
-    inquirer
-        .prompt([
-            {type: 'list',
-            name: 'newMember',
-            message: 'Which team member would you like to add?',
-            choices: ['Intern', 'Engineer', 'None']
-            },
-
-        ]).then (function(answers) {
-            switch(answers.newMember) {
-                case 'Intern':
-                    createIntern();
-                break;
-                case 'Engineer':
-                    createEngineer();
-                break;
-                case 'None':
-                    const employeeProfile = createSite(teamArray);
-                    // writeToFile('index.html', employeeProfile);
-                    fs.writeFile(path.join(process.cwd(), '/dist/assets', 'index.html'), employeeProfile, function(err) {
-                        if (err) {
-                          return err;
-                        } else {
-                          console.log('Your index.html has been succesfully generated.')
-                        };
-                      });
-                      break;
-            };
-        });
-};
-
-function writeToFile(fileName, data) {
-fs.writeFile(path.join(process.cwd(), '/dist/assets', fileName), data, function(err) {
-    if (err) {
-      return err;
-    } else {
-      console.log('Your index.html has been succesfully generated.')
-    };
-  })
+  inquirer
+    .prompt([
+      {
+        type: "list",
+        name: "newMember",
+        message: "Which team member would you like to add?",
+        choices: ["Intern", "Engineer", "None"],
+      },
+    ])
+    .then(function (answers) {
+      switch (answers.newMember) {
+        case "Intern":
+          createIntern();
+          break;
+        case "Engineer":
+          createEngineer();
+          break;
+        case "None":
+          const employeeProfile = createSite(teamArray);
+          // writeToFile('index.html', employeeProfile);
+          fs.writeFile(
+            path.join(process.cwd(), "/dist/assets", "index.html"),
+            employeeProfile,
+            function (err) {
+              if (err) {
+                return err;
+              } else {
+                console.log("Your index.html has been succesfully generated.");
+              }
+            }
+          );
+          break;
+      }
+    });
 };
 
 const prompt = new Prompt();
